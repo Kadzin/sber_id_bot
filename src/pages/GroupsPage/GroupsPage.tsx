9f541f59f666d98cd40f7bb5c5f708fb1e6780ca
SENyuskhaev <senyuskhaev@sberbank.ru>
2022-06-20 15:52:43 +0300

    v1.2. Новый алгоритм цветов шапки карточек

--- src/pages/GroupsPage/GroupsPage.tsx
+++ src/pages/GroupsPage/GroupsPage.tsx
@@ -52,17 +52,11 @@ const GroupsPage = () => {
     const generateImage = () => {
         const direction = Math.round(Math.random() * 360 / 4 + 45);
 
-        const r1 = Math.round(Math.random() * 255);
-        const g1 = Math.round(Math.random() * 255);
-        const b1 = Math.round(Math.random() * 255);
-        const a1 = Math.round(Math.random() * 10) / 10;
+        const r1 = Math.round(Math.random() * (200 - 50) + 50);
+        const g1 = Math.round(Math.random() * (200 - 50) + 50);
+        const b1 = Math.round(Math.random() * (200 - 50) + 50);
 
-        const r2 = Math.round(Math.random() * 255);
-        const g2 = Math.round(Math.random() * 255);
-        const b2 = Math.round(Math.random() * 255);
-        const a2 = Math.round(Math.random() * 10) / 10;
-
-        return `linear-gradient(${direction}deg, rgba(${r1},${g1},${b1},${a1}), rgba(${r2},${g2},${b2},${a2}))`
+        return `linear-gradient(${direction}deg, rgba(${r1},${g1},${b1},1), rgba(${r1},${g1},${b1},.2))`
     }
 
 
